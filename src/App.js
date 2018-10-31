42367474d9e9e7b464b229d06a400e8c7b5cdac8
Okay Onbaşı <okayonbasi@gmail.com>
2018-10-31 14:53:33 +0200

    connect ile componentden store'a baglantı saglandı

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux';
 
 class App extends Component {
   render() {
@@ -25,4 +26,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
