cec79dc819f296705a85384063999ae4434ea301
Okay Onbaşı <okayonbasi@gmail.com>
2018-10-31 14:04:09 +0200

    created store

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+
+function reducer(){
+    return "state";
+}
+const store = createStore(reducer);
+console.log(store.getState());
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
