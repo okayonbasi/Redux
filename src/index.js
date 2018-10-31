9a487c8f2d5089f8488677b377f87b0cb2c356d8
Okay Onbaşı <okayonbasi@gmail.com>
2018-10-31 14:26:31 +0200

    used combineReducers

--- src/index.js
+++ src/index.js
@@ -3,35 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
+import {combineReducers, createStore} from 'redux';
 
-function reducer(state, action){
-    if(action.type === 'changeTheState'){
-        return action.payload.newState;
-    }
-    return "state";
+function productReducer(state = [], action){
+    return state;
 }
-const store = createStore(reducer);
-console.log(store.getState());
+function userReducer(state="", action){
+    return state;
+}
 
-const action = {
-    type:"changeTheState",
-    payload:{
-        newState:"my new state"
-    }
-};
-const action2 = {
-    type:"changeTheState",
-    payload:{
-        newState:"my new state 2"
-    }
-};
-store.subscribe(() => {
-    console.log("store updated", store.getState());
-});
-store.dispatch(action);
-store.dispatch(action2);
-
-console.log(store.getState());
+const rootReducer = combineReducers({
+    user: userReducer,
+    products: productReducer
+})
+const store = createStore(rootReducer);
+console.log(store.getState())
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
